83ca495109eb65372e3ca6ee27477e15fcd1a96b
Leilei <coleflowersma@gmail.com>
2020-04-18 18:14:20 +0800

    add test file

---
+++ tests/test.php
@@ -0,0 +1,22 @@
+<?php
+
+class TestPrice
+{
+    private $id = 1;
+    private $name = 2;
+    public $language = '';
+    var $age;
+
+    public function setId($a)
+    {
+        $this->a = $a;
+    }
+
+    public static function sayHello()
+    {
+    }
+
+    public function printTxt($txt = '')
+    {
+    }
+}
